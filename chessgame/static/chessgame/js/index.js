27a49114547c56fc21a1ad6a676da7f77bb4d2c8
SantosLaprida <lapridasantos95@gmail.com>
2024-09-27 20:59:25 -0600

    First commit after break

--- chessgame/static/chessgame/js/index.js
+++ chessgame/static/chessgame/js/index.js
@@ -1,106 +1,91 @@
-
-import { getPieceImageUrl, getPieceImageUrlSVG, processFen } from "./chessUtils.js";
-
+import {
+  getPieceImageUrl,
+  getPieceImageUrlSVG,
+  processFen,
+} from "./chessUtils.js";
 
 let board = document.getElementById("chessBoard");
-
 let squares = [];
 
-
 for (let i = 0; i < 64; i++) {
-	let cell = document.createElement("div");
+  let cell = document.createElement("div");
 
-	cell.className = (Math.floor(i / 8) % 2 === i % 2) ? "white" : "black";
-	
-	board.appendChild(cell);
-	squares.push(cell);
+  cell.className = Math.floor(i / 8) % 2 === i % 2 ? "white" : "black";
+
+  board.appendChild(cell);
+  squares.push(cell);
 }
 
 let expand = () => {
-	let board = document.getElementById("chessBoard");
-	if (board.style.width < "80vh") {
-		board.style.width = "85vh";
-		board.style.height = "85vh";
-
-	} else {
-		board.style.width = "70vh";
-		board.style.height = "70vh";
-	}
+  let board = document.getElementById("chessBoard");
+  if (board.style.width < "80vh") {
+    board.style.width = "85vh";
+    board.style.height = "85vh";
+  } else {
+    board.style.width = "70vh";
+    board.style.height = "70vh";
+  }
 };
 
+fetch("/initial-board-fen")
+  .then((response) => response.json())
+  .then((data) => {
+    // send the fen (data.fen) to the processFen function to get the board state
+    const board_state = processFen(data.fen);
 
+    // Loop through the board data (dictionary with keys as positions and values as piece info (array with piece type and alliance))
+    for (let position in board_state) {
+      //console.log("Position: " + position);
+      // Create pieceInfo variable from the array
+      let pieceInfo = board_state[position]; // [pieceType, alliance]
 
+      // Create the variable imageUrl by calling the getPieceImageUrl function with the piece type and alliance
+      let imageUrl = getPieceImageUrlSVG(pieceInfo[0], pieceInfo[1]);
 
+      // Set the background image of the board cell
+      let cell = board.children[position];
+      cell.style.backgroundImage = `url('${imageUrl}')`;
+    }
 
-fetch("/initial-board-fen") 
-	.then((response) => response.json())
-	.then((data) => {	
+    window.chessBoard = board;
+  })
+  .catch((error) =>
+    console.error("There was an error fetching the initial board:", error)
+  );
 
-			// send the fen (data.fen) to the processFen function to get the board state
-			const board_state = processFen(data.fen);
-
-			// Loop through the board data (dictionary with keys as positions and values as piece info (array with piece type and alliance))
-			for (let position in board_state) {
-
-				//console.log("Position: " + position);
-				// Create pieceInfo variable from the array
-				let pieceInfo = board_state[position]; // [pieceType, alliance]
-
-				// Create the variable imageUrl by calling the getPieceImageUrl function with the piece type and alliance
-				let imageUrl = getPieceImageUrlSVG(pieceInfo[0], pieceInfo[1]);
-
-				// Set the background image of the board cell
-				let cell = board.children[position];
-				cell.style.backgroundImage = `url('${imageUrl}')`;
-				
-			}
-			
-
-			window.chessBoard = board; 
-
-	})
-	.catch((error) =>
-		console.error("There was an error fetching the initial board:", error),
-	);
-	
 //*************************************// //TESTING
 
-
-
-
-
 function darVueltaTablero() {
+  console.log("Flip board called");
 
-	console.log("Flip board called");
+  let chessBoard = document.getElementById("chessBoard");
+  chessBoard.classList.toggle("dadoVuelta");
 
+  // Recorre los hijos del tablero
+  for (let i = 0; i < chessBoard.children.length; i++) {
+    let tile = chessBoard.children[i];
 
-	let chessBoard = document.getElementById("chessBoard");
-	chessBoard.classList.toggle("dadoVuelta");
+    // Obtener la imagen de fondo dentro de la casilla
+    let imageUrl = tile.style.backgroundImage;
 
-	// Recorre los hijos del tablero
-	for (let i = 0; i < chessBoard.children.length; i++) {
-		let tile = chessBoard.children[i];
-
-		// Obtener la imagen de fondo dentro de la casilla
-		let imageUrl = tile.style.backgroundImage;
-
-		// Si hay una imagen de fondo, rotarla
-		if (imageUrl) {
-			tile.style.transform =
-				tile.style.transform === "rotate(180deg) rotateY(180deg)"
-					? "rotate(0deg) rotateY(0deg)"
-					: "rotate(180deg) rotateY(180deg)";
-		}
-	}
+    // Si hay una imagen de fondo, rotarla
+    if (imageUrl) {
+      tile.style.transform =
+        tile.style.transform === "rotate(180deg) rotateY(180deg)"
+          ? "rotate(0deg) rotateY(0deg)"
+          : "rotate(180deg) rotateY(180deg)";
+    }
+  }
 }
 // Add the event listener at the end of the file
-document.querySelector('.btnRotate').addEventListener('click', darVueltaTablero);
-
+document
+  .querySelector(".btnRotate")
+  .addEventListener("click", darVueltaTablero);
 
 // function checkIfSquareShouldBeHighlighted(squareIndex, callback) {
-		
+
 // 	//let boardState = globalBoardState;
-	
+
 // 	fetch('/check-highlight/', {
 //         method: 'POST',
 //         headers: {
@@ -131,12 +116,11 @@ document.querySelector('.btnRotate').addEventListener('click', darVueltaTablero)
 // CURRENTLY DOING
 //***************************************************************************************************
 //***************************************************************************************************
- 
+
 let sourceSquare = null;
 let destinationSquare = null;
 
 //console.log(squares);
-
 
 // // Right-click handling
 // squares.forEach((square, index) => {
@@ -147,62 +131,52 @@ let destinationSquare = null;
 // });
 
 squares.forEach((square, index) => {
+  // Right-click handling
 
-	// Right-click handling
+  square.addEventListener("contextmenu", function (event) {
+    event.preventDefault();
+    console.log("rfeahedhstgrhtgrg");
 
-	square.addEventListener('contextmenu', function(event) {
-		event.preventDefault();
-		console.log("rfeahedhstgrhtgrg");
+    if (sourceSquare != null) {
+      squares.forEach((s, i) => {
+        console.log("Removing highlight from square: " + i);
+        s.classList.remove("highlight");
+      });
+      sourceSquare = null;
+      destinationSquare = null;
+    }
+  });
 
-		if(sourceSquare != null){
-			squares.forEach((s, i) => {
-				console.log("Removing highlight from square: " + i);
-				s.classList.remove('highlight');
-			});
-			sourceSquare = null;
-			  destinationSquare = null;
-		}
-	});
+  square.addEventListener("click", function (event) {
+    if (event.button !== 0) return;
 
+    // Left-click handling
+    if (sourceSquare === null) {
+      sourceSquare = index;
+      squares[index].classList.add("highlight");
+      console.log("Source square selected: " + index);
+    } else if (sourceSquare === index) {
+      // Unselect if the same square is clicked again
+      squares[index].classList.remove("highlight");
+      sourceSquare = null;
+      console.log("Source square unselected: " + index);
+    } else {
+      destinationSquare = index;
+      squares[index].classList.add("highlight");
+      console.log("Destination square selected: " + index);
 
-    square.addEventListener('click', function(event) {
+      // Send move to backend or handle it
+      // Reset after processing the move
 
-		if (event.button !== 0) return;
-
-		// Left-click handling
-		if (sourceSquare === null) {
-			sourceSquare = index;
-			squares[index].classList.add('highlight');
-			console.log("Source square selected: " + index);
-
-
-		} else if (sourceSquare === index) {
-			// Unselect if the same square is clicked again
-			squares[index].classList.remove('highlight');
-			sourceSquare = null;
-			console.log("Source square unselected: " + index);
-
-
-		} else {
-
-			destinationSquare = index;
-			squares[index].classList.add('highlight');
-			console.log("Destination square selected: " + index);
-
-			// Send move to backend or handle it
-			// Reset after processing the move
-
-			squares[sourceSquare].classList.remove('highlight');
-			squares[destinationSquare].classList.remove('highlight');
-			sourceSquare = null;
-			destinationSquare = null;
-
-		}
- 
-    });
+      squares[sourceSquare].classList.remove("highlight");
+      squares[destinationSquare].classList.remove("highlight");
+      sourceSquare = null;
+      destinationSquare = null;
+    }
+  });
 });
 
 // Prevent the default right-click menu
-document.getElementById("chessBoard").addEventListener('contextmenu', event => event.preventDefault());
-
-
+document
+  .getElementById("chessBoard")
+  .addEventListener("contextmenu", (event) => event.preventDefault());
